d6aba9aa2dd0b7999e4658b90aed00de92bf2438
Adishwar Rishi <adiswa123@gmail.com>
2026-02-10 19:51:11 +1100

    feat: add HeroUI Native component library

--- README.md
+++ README.md
@@ -48,3 +48,7 @@ Join our community of developers creating universal apps.
 
 - [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
 - [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+
+## Known Divergence from Expo setup
+
+- react-native-svg is at 15.15.3 instead of 15.15.1 due to a bug that imported node buffer in previous versions.
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,23 @@
-import "@/global.css";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { HeroUINativeProvider } from "heroui-native";
 import React from "react";
 import { useColorScheme } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { AnimatedSplashOverlay } from "@/components/animated-icon";
 import AppTabs from "@/components/app-tabs";
+import "@/global.css";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          <AnimatedSplashOverlay />
+          <AppTabs />
+        </ThemeProvider>
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
   );
 }
